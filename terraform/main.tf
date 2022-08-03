0f0e462ac866cd65f3faaedf2e6b39a7957537bd
Samuel Akinola <34632633+Eldorado7621@users.noreply.github.com>
2022-08-03 21:40:55 +0200

    count variable added to the resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,15 +4,15 @@ provider "azurerm" {
 
 # image create by the packer script
 data "azurerm_image" "web" {
-  name                = "project1-packer-image"
-  resource_group_name = var.packer_resource_group
+ name                = "project1-packer-image"
+resource_group_name = var.packer_resource_group
 }
 
 #resource group
 resource "azurerm_resource_group" "main" {
-  name     = "${var.prefix}-rg"
-  location = var.location
-}
+  name     = azurerm_resource_group.main.name
+  location = azurerm_resource_group.main.location
+ }
 #virtual network
 resource "azurerm_virtual_network" "main" {
   name                = "${var.prefix}-network"
@@ -44,47 +44,51 @@ resource "azurerm_network_security_rule" "rule1" {
   direction                   = "Outbound"
   access                      = "Allow"
   protocol                    = "*"
-  source_port_range           = azurerm_virtual_network.main.address_space
-  destination_port_range      = azurerm_virtual_network.main.address_space
+  source_port_range           = "*"
+  destination_port_range      = "8080"
   source_address_prefix       = "VirtualNetwork"
   destination_address_prefix  = "VirtualNetwork"
   resource_group_name         = azurerm_resource_group.main.name
   network_security_group_name = azurerm_network_security_group.main.name
 }
+
 resource "azurerm_network_security_rule" "rule2" {
-    name                         = "AllowInboundFromVMsInSubnet"
-    description                  = "This rule allow the inbound traffic inside the same virtual network.(subnet)"
-    priority                     = 101
-    direction                    = "Inbound"
-    access                       = "Allow"
-    protocol                     = "*"
-    source_port_ranges           = azurerm_virtual_network.main.address_space
-    destination_port_ranges      = azurerm_virtual_network.main.address_space
-    source_address_prefix        = "VirtualNetwork"
-    destination_address_prefix   = "VirtualNetwork"
-    resource_group_name          = azurerm_resource_group.main.name
-    network_security_group_name  = azurerm_network_security_group.main.name
+   name                         = "AllowInboundFromVMsInSubnet"
+   description                  = "This rule allow the inbound traffic inside the same virtual network.(subnet)"
+   priority                     = 101
+   direction                    = "Inbound"
+   access                       = "Allow"
+   protocol                     = "*"
+   source_port_ranges           = azurerm_virtual_network.main.address_space
+   destination_port_ranges      = azurerm_virtual_network.main.address_space
+   source_address_prefix        = "VirtualNetwork"
+   destination_address_prefix   = "VirtualNetwork"
+   resource_group_name          = azurerm_resource_group.main.name
+   network_security_group_name  = azurerm_network_security_group.main.name
 }
+
+
 # Create security rules
 resource "azurerm_network_security_rule" "rule3" {
-    name                         = "DenyAllInbound"
-    description                  = "This rule denies all the inbound traffic from the internet."
-    priority                     = 100
-    direction                    = "Inbound"
-    access                       = "Deny"
-    protocol                     = "*"
-    source_port_range            = "*"
-    destination_port_range       = "*"
-    source_address_prefix        = "*"
-    destination_address_prefix   = "*"
-    resource_group_name          = azurerm_resource_group.main.name
-    network_security_group_name  = azurerm_network_security_group.main.name
+  name                        = "DenyAllInbound"
+  description                 = "This rule denies all the inbound traffic from the internet."
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Deny"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.main.name
+  network_security_group_name = azurerm_network_security_group.main.name
 }
 
 
 # Create network interface
 resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
+  count              = var.nb_vms
+  name                 = "${var.prefix}-${count.index}-nic"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
 
@@ -119,23 +123,29 @@ resource "azurerm_lb" "main" {
 
 # The load balancer will use this backend pool
 resource "azurerm_lb_backend_address_pool" "main" {
-  resource_group_name = azurerm_resource_group.main.name
-  loadbalancer_id     = azurerm_lb.main.id
-  name                = "${var.prefix}-lb-backend-pool"
+
+  loadbalancer_id = azurerm_lb.main.id
+  name            = "${var.prefix}-lb-backend-pool"
 }
 
 # We associate the LB with the backend address pool
 resource "azurerm_network_interface_backend_address_pool_association" "main" {
-  network_interface_id    = azurerm_network_interface.main.id
+  count              = var.nb_vms
+  network_interface_id    = azurerm_network_interface.main[count.index].id
   ip_configuration_name   = "internal"
   backend_address_pool_id = azurerm_lb_backend_address_pool.main.id
 }
 
 # Create virtual machine availability set
 resource "azurerm_availability_set" "main" {
-  name                = "${var.prefix}-aset"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  name                = "${var.prefix}-aset"  
+  resource_group_name             = azurerm_resource_group.main.name
+  location                        = azurerm_resource_group.main.location
+
+  tags = {
+    project = "udacity1"
+    environment = "development"
+  }
 }
 
 # Create the virtual machines
@@ -145,13 +155,15 @@ resource "azurerm_linux_virtual_machine" "main" {
   resource_group_name             = azurerm_resource_group.main.name
   location                        = azurerm_resource_group.main.location
   size                            = "Standard_B1ls"
-  admin_username                  = "${var.username}"
-  admin_password                  = "${var.password}"
+  admin_username                  = var.username
+  admin_password                  = var.password
   disable_password_authentication = false
-  network_interface_ids = [element(azurerm_network_interface.main.*.id, count.index)]
-  availability_set_id = azurerm_availability_set.main.id
+  network_interface_ids           = [element(azurerm_network_interface.main.*.id, count.index)]
+  availability_set_id             = azurerm_availability_set.main.id
+
 
   source_image_id = data.azurerm_image.web.id
+ 
 
   os_disk {
     storage_account_type = "Standard_LRS"
